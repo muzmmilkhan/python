6afa9fc2ad7136f29d5ae17336697745bbd6d4a9
Muzmmil Pathan <muzmmil_pathan@apple.com>
2025-07-13 18:24:13 +0530

    Modules & Packages in Python

---
+++ 5-Modules/test.py
@@ -0,0 +1,11 @@
+from package.maths import *
+from package.subpackage.mul import multiply
+
+# Example usage of the functions
+print(add(5, 3))  # Output: 8
+print(subtract(10, 4))  # Output: 6
+print(multiply(2, 6))  # Output: 12
+print(divide(8, 2))  # Output: 4.0  
+
+print(multiply(3, 7))  # Output: 21
+print(multiply(4, 5))  # Output: 20
